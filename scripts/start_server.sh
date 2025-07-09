8b7e24626ed0b47c5199e3217f806a2898cc60b8
samikshakotwal <89957781+samikshakotwal@users.noreply.github.com>
2025-07-09 17:17:14 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+sudo docker stop react-frontend || true
+sudo docker rm react-frontend || true
+sudo docker stop node-backend || true
+sudo docker rm node-backend || true
+
 cd /home/ubuntu/3-tier-project-1
 
-sudo run -it -d -p 81:80 web
-sudo run -it -d -p 4000:4000 app
+sudo run -it -d --name react-forntend -p 81:80 web
+sudo run -it -d -p --name react-backend 4000:4000 app
 
