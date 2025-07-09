a63d38efaae1439e0f173f915eae1f1f48ccee88
samikshakotwal <89957781+samikshakotwal@users.noreply.github.com>
2025-07-09 13:19:21 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-echo "Linking custom nginx config..."
-sudo ln -sf /etc/nginx/sites-available/react-backend /etc/nginx/sites-enabled/react-backend
-
-# Optional: Remove default config if needed
-sudo rm -f /etc/nginx/sites-enabled/default
-
-echo "Testing nginx config..."
-sudo nginx -t
-
-echo "Reloading nginx..."
-sudo systemctl reload nginx
+sudo cp /etc/nginx/sites-available/react-backend /etc/nginx/sites-enabled/
